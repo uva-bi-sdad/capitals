0f0dcd1034c9d817f70e388b3e8b9be6aa49e679
mavoss16 <mavoss16@gmail.com>
2020-08-26 23:27:12 -0500

    Annotation of 03_getfsa.R

--- src/natural/03_getfsa.R
+++ src/natural/03_getfsa.R
@@ -3,15 +3,20 @@ library(magrittr)
 library(readxl)
 library(readr)
 
+# Read in USDA-FSA Conservation Reserve Program data
 fsa = read_xlsx("data/natural/nat_fsa_2020.xlsx", skip = 3)
 
-names(fsa)
-
+# Get rid of first row, which has secondary column names
 fsa = fsa[-1,]
 
+# Keep only data for the three states of interest
 fsa %<>% filter(STATE %in% c("IOWA", "OREGON", "VIRGINIA"))
 
+# Select only the columns of interest
 fsa %<>% select(FIPS, STATE, COUNTY, `RARE AND\r\nDECLINING\r\nHABITAT\r\n(CP25)`, `POLLINATOR\r\nHABITAT 8/\r\n(CP42)`, `STATE\r\nACRES FOR\r\nWILDLIFE\r\nENHANCEMENT\r\n(CP38)`)
 
+# Change column names to make them less unwieldy
 names(fsa) = c("FIPS", "STATE", "COUNTY", "rare_hab", "pol_hab", "wildlife")
 
+# Write dataframe to rds file
+write_rds(fsa, "data/natural/nat_fsa_2020.rds")
