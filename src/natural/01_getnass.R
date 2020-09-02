1c8a2c7dd08ef64ab7cfa8461dec98c51ba5cbf1
Teja Pristavec <tp2sk@virginia.edu>
2020-09-02 15:44:23 -0400

    Updated NASS missingness

--- src/natural/01_getnass.R
+++ src/natural/01_getnass.R
@@ -3,11 +3,17 @@ library(dplyr)
 library(magrittr)
 library(janitor)
 library(tidycensus)
+library(stringr)
+
+# Glossary: https://quickstats.nass.usda.gov/src/glossary.pdf. 
+# (D): Withheld to avoid disclosing data for individual operations.
+# (Z): Less than half the rounding unit.
+# These are legitimate NAs and should be coded as such.
 
 # Read in USDA-NASS data files
 ag_land = read_csv("data/natural/nat_nass_2017_aglandtotal.csv") %>% clean_names()
-agritourism = read_csv("data/natural/nat_nass_2017_agritourism.csv", na = c("(D)", "")) %>% clean_names()
-forestry = read_csv("data/natural/nat_nass_2017_forestry.csv", na = c("(D)", "", "(Z)")) %>% clean_names()
+agritourism = read_csv("data/natural/nat_nass_2017_agritourism.csv") %>% clean_names()
+forestry = read_csv("data/natural/nat_nass_2017_forestry.csv") %>% clean_names()
 
 # Create new columns with accurate names and select relevant columns
 ag_land %<>% mutate(acres_operated = value) %>% select(state, state_ansi, county, county_ansi, acres_operated)
@@ -42,8 +48,14 @@ ag_land$GEOID <- paste0(ag_land$state_ansi, ag_land$county_ansi)
 nass_data = full_join(ag_land, agritourism, by = c("GEOID", "state", "state_ansi", "county", "county_ansi"))
 nass_data = full_join(nass_data, forestry, by = c("GEOID", "state", "state_ansi", "county", "county_ansi"))
 
+# Counties without data (NOT counties with (D) or (Z)) have 0 operations/revenue and should be recoded to 0 as a legitimate value.
+nass_data <- nass_data %>% mutate(agritourism_revenue = ifelse(is.na(agritourism_revenue), 0, agritourism_revenue),
+                                  forestry_revenue = ifelse(is.na(forestry_revenue), 0,  forestry_revenue))
+  
 # Read in County area data
 counties = read_csv("data/natural/nat_census_2019_area.csv")
+ 
+# Independent cities don't have data and should stay NA.
 
 # Keep counties of interest
 counties %<>% filter(STATEFP %in% c(19, 41, 51))
@@ -54,6 +66,19 @@ counties %<>% mutate(ALAND_acres = ALAND * 0.00024711, AWATER_acres = AWATER * 0
 # Join nass_data to county data
 nass_data = left_join(counties, nass_data, by = c("GEOID", "STATEFP" = "state_ansi", "COUNTYFP" = "county_ansi"))
 nass_data = nass_data %>% select(STATEFP, COUNTYFP, GEOID, NAME, acres_operated, agritourism_revenue, forestry_revenue, ALAND_acres, AWATER_acres)
+
+# Counties with "", (D), (Z) are true NAs and can now be recoded to NA.
+nass_data <- nass_data %>% mutate(agritourism_revenue = ifelse(agritourism_revenue == "(D)", NA, agritourism_revenue),
+                                  agritourism_revenue = ifelse(agritourism_revenue == "", NA, agritourism_revenue),
+                                  forestry_revenue = ifelse(forestry_revenue == "(D)", NA, forestry_revenue),
+                                  forestry_revenue = ifelse(forestry_revenue == "", NA, forestry_revenue),
+                                  forestry_revenue = ifelse(forestry_revenue == "(Z)", NA, forestry_revenue))
+
+nass_data$agritourism_revenue <- str_remove_all(nass_data$agritourism_revenue, ",")
+nass_data$forestry_revenue <- str_remove_all(nass_data$forestry_revenue, ",")
+
+nass_data$agritourism_revenue <- as.numeric(nass_data$agritourism_revenue)
+nass_data$forestry_revenue <- as.numeric(nass_data$forestry_revenue)
 
 # Create new columns accurately named and adjusted for area
 nass_data %<>% mutate(total_area = ALAND_acres + AWATER_acres)
