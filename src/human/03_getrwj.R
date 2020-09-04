99ff6157d92167b22475c367679315b8a58f6275
Teja Pristavec <tp2sk@virginia.edu>
2020-09-04 10:40:09 -0400

    Updating human capital with reading and math

--- src/human/03_getrwj.R
+++ src/human/03_getrwj.R
@@ -2,19 +2,26 @@ library(naniar)
 library(dplyr)
 library(readxl)
 library(readr)
+library(sf)
+library(tidycensus)
 
 # also get math and reading scores from here! it's on the additional measures tab. Reading scores: ER, Math scores: EW
 
 
 # read in RWJ County Health Rankings data ----------------------------------
 
-ia <- read_excel("./rivanna_data/human/human_rwj_2020 - Iowa.xlsx", sheet = "Ranked Measure Data", skip=1)
-or <- read_excel("./rivanna_data/human/human_rwj_2020 - Oregon.xlsx", sheet = "Ranked Measure Data", skip=1)
-va <- read_excel("./rivanna_data/human/human_rwj_2020 - Virginia.xlsx", sheet = "Ranked Measure Data", skip=1)
+ia1 <- read_excel("./rivanna_data/human/human_rwj_2020 - Iowa.xlsx", sheet = "Ranked Measure Data", skip = 1)
+or1 <- read_excel("./rivanna_data/human/human_rwj_2020 - Oregon.xlsx", sheet = "Ranked Measure Data", skip = 1)
+va1 <- read_excel("./rivanna_data/human/human_rwj_2020 - Virginia.xlsx", sheet = "Ranked Measure Data", skip = 1)
+
+ia2 <- read_excel("./rivanna_data/human/human_rwj_2020 - Iowa.xlsx", sheet = "Additional Measure Data", skip = 1)
+or2 <- read_excel("./rivanna_data/human/human_rwj_2020 - Oregon.xlsx", sheet = "Additional Measure Data", skip = 1)
+va2 <- read_excel("./rivanna_data/human/human_rwj_2020 - Virginia.xlsx", sheet = "Additional Measure Data", skip = 1)
+
 
 # select only the indicators we want -----------------------------
 
-ia <- ia %>%
+ia1 <- ia1 %>%
   select(FIPS, State, County, 
          `Average Number of Physically Unhealthy Days`,
          `Average Number of Mentally Unhealthy Days`,
@@ -24,7 +31,13 @@ ia <- ia %>%
          `% Single-Parent Households`
          )
 
-or <- or %>%
+ia2 <- ia2 %>%
+  select(FIPS, State, County, 
+         `Average Grade Performance...148`, #reading
+         `Average Grade Performance...153`  #math
+  )
+
+or1 <- or1 %>%
   select(FIPS, State, County, 
          `Average Number of Physically Unhealthy Days`,
          `Average Number of Mentally Unhealthy Days`,
@@ -34,7 +47,13 @@ or <- or %>%
          `% Single-Parent Households`
   )
 
-va <- va %>%
+or2 <- or2 %>%
+  select(FIPS, State, County, 
+         `Average Grade Performance...148`,  #reading
+         `Average Grade Performance...153`   #math
+  )
+
+va1 <- va1 %>%
   select(FIPS, State, County, 
          `Average Number of Physically Unhealthy Days`,
          `Average Number of Mentally Unhealthy Days`,
@@ -42,11 +61,21 @@ va <- va %>%
          `Primary Care Physicians Rate`,
          `Mental Health Provider Rate`,
          `% Single-Parent Households`
+  )
+
+va2 <- va2 %>%
+  select(FIPS, State, County, 
+         `Average Grade Performance...148`, #reading
+         `Average Grade Performance...153`  #math
   )
 
 # create one dataframe for the 3 states --------------------------
 
-rwj <- rbind(ia, or, va)
+iowa <- left_join(ia1, ia2, by = c("FIPS", "State", "County"))
+oregon <- left_join(or1, or2, by = c("FIPS", "State", "County"))
+virginia <- left_join(va1, va2, by = c("FIPS", "State", "County"))
+
+rwj <- rbind(iowa, oregon, virginia)
 
 # add geometry data from ACS ------------------------------------
 
@@ -70,12 +99,19 @@ rwj_geo <- rwj_geo %>%
          hum_pctnophys = `% Physically Inactive`,
          hum_ratepcp = `Primary Care Physicians Rate`,
          hum_ratementalhp = `Mental Health Provider Rate`,
-         hum_pctsngparent = `% Single-Parent Households`)
+         hum_pctsngparent = `% Single-Parent Households`,
+         hum_reading = `Average Grade Performance...148`,
+         hum_math = `Average Grade Performance...153`)
 
 
 # missingness check --------------------------------------
 
 miss_var_summary(rwj_geo)  # Out of 268, 6 missing in hum_ratepcp, 14 missing in hum_ratementalhp
+
+# 1 hum_reading           28    10.4 
+# 2 hum_math              26     9.70
+# 3 hum_ratementalhp      14     5.22
+# 4 hum_ratepcp            6     2.24
 
 # write ----------------------------------
 
