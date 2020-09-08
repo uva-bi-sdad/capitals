0a45628e2010702d3d737ad2e7471403c9517884
Morgan Stockham <morganstockham@gmail.com>
2020-09-08 13:44:26 -0400

    finished census 2020 but census 2010 is being difficult.

---
+++ src/social/04_getcensus20.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(naniar)
+library(readr)
+library(janitor)
+library(sf)
+
+#
+# Get data ------------------------------------------------------------------------
+#
+
+data <- read.csv("./rivanna_data/social/soc_census_2020.csv")
+
+# CRRALL - cummulative self response rate, CAVG - cummulative average overall response rate
+
+data <- data %>%
+  transmute(GEOID = GEO_ID,
+            overall_census_rate = CRRALL,
+            overall_avg_census_rate = CAVG) %>%
+  separate(GEOID, c("place", "GEOID"), "US")
+
+# add geometry data from ACS ------------------------------------
+
+acs <- readRDS("./rivanna_data/social/soc_acs_2018.rds")
+
+acs <- acs %>%
+  select(STATEFP, COUNTYFP, GEOID, geometry)
+
+data$GEOID <- as.character(data$GEOID)
+
+data_geo <- left_join(acs, data, by = c("GEOID"))
+
+data_geo$STATEFP <- substr(data_geo$GEOID,1,2)
+data_geo$COUNTYFP <-substr(data_geo$GEOID,3,5)
+
+data_geo$STATEFP <- as.character(data_geo$STATEFP)
+data_geo$COUNTYFP <- as.character(data_geo$COUNTYFP)
+
+
+# check missingness ----------------------------------
+
+miss_var_summary(data_geo)
+
+#
+# Write ------------------------------------------------------------------------
+#
+
+write_rds(data_geo, "./rivanna_data/social/soc_census_2020.rds")
---
+++ src/social/05_getcensus10.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(naniar)
+library(data.table)
+library(sf)
+
+#
+# Get data ------------------------------------------------------------------------
+#
+
+data_pre <- read.table("./rivanna_data/social/soc_census_2010.txt", sep = "|", header = F)
+
+data_1 <- data_pre %>%
+  transmute(GEOID = V1,
+            name = V3,
+            type = V5,
+            participation_rate = V9) %>%
+  filter(type == "County ")
+
+data_1 <- data_1[-1,]
+
+data_2 <- data_1 %>%
+  separate(name, c("county", "state"), ", ") 
+
+data_3 <- data_2 %>%
+  filter(state %in% c("VA ", "IA ","OR "))
+data <- data_3 
+
+data$STATEFP <- substr(data$GEOID,1,2)
+data$COUNTYFP <- substr(data$GEOID,3,5)
+
+data$STATEFP <- as.character(data$STATEFP)
+data$COUNTYFP <- as.character(data$COUNTYFP)
+
+data$GEOID <- as.character(data$GEOID)
+acs$GEOID <- as.character(acs$GEOID)
+
+
+# add geometry data from ACS ------------------------------------
+
+acs <- readRDS("./rivanna_data/social/soc_acs_2018.rds")
+
+acs <- acs %>%
+  select(STATEFP, COUNTYFP, GEOID, geometry)
+
+data_geo <- left_join(acs, data, by = c("STATEFP", "COUNTYFP", "GEOID"))
+
+# check missingness ----------------------------------
+
+miss_var_summary(data_geo)
+
+#
+# Write ------------------------------------------------------------------------
+#
+
+write_rds(data_geo, "./rivanna_data/social/soc_census_2010.rds")
