2496c48a296a194aa634bb44b6e867d09f281663
brandonleekramer <brandonleekramer@gmail.com>
2020-11-06 17:02:07 -0500

    updates to aggregation in built capital

--- src/built/10_assemble_county.R
+++ src/built/10_assemble_county.R
@@ -36,6 +36,7 @@ data <- left_join(data, hifld_data, by = c("STATEFP", "COUNTYFP", "GEOID", "NAME
 data <- left_join(data, hrsa_data, by = c("STATEFP", "GEOID", "NAME.y")) 
 data <- left_join(data, rurality, by = c("GEOID" = "fips2010", "NAME.y" = "county_name"))
 
+
 #
 # De-select columns -----------------------------------------------------------------------
 #
@@ -77,24 +78,55 @@ pct_miss_var(data)
 # Composite Creation 
 #
 
-data %>%
-  mutate(cell_tower_adj = (cell_tower_count / ALAND),
-         publibs_adj = (built_publibs / ALAND),
-         lib_avcomputers_adj = (built_lib_avcomputers / COUNTYPOP) * 100,
-         lib_computeruse_adj = (built_lib_computeruse / COUNTYPOP) * 100
-         ) %>% 
-  select(COUNTYFP, cell_tower_adj, publibs_adj, lib_avcomputers_adj, lib_computeruse_adj)
+data <- data %>% # telecom 
+  mutate(ALAND_acres = ALAND * 0.00024711, 
+         AWATER_acres = AWATER * 0.00024711,
+         total_area = ALAND_acres + AWATER_acres,
+         pcp_facs = replace_na(pcp_facs, 0),
+         nhsc_facs = replace_na(nhsc_facs, 0),
+         dental_facs = replace_na(dental_facs, 0),
+         mentalhealth_facs = replace_na(mentalhealth_facs, 0),
+         built_publibs = replace_na(built_publibs, 0),
+         #built_lib_avcomputers= replace_na(built_lib_avcomputers, 0),
+         #built_lib_computeruse = replace_na(built_lib_computeruse, 0),
+         built_comm_airports = replace_na(built_comm_airports, 0),
+         built_noncomm_airports = replace_na(built_noncomm_airports, 0),
+         cell_tower_adj = (cell_tower_count / ALAND_acres),
+         publibs_adj = (built_publibs / COUNTYPOP),
+         lib_avcomputers_adj = (built_lib_avcomputers / COUNTYPOP * 10000) * 100,
+         lib_computeruse_adj = (built_lib_computeruse / COUNTYPOP * 10000) * 100,
+         #transportation
+         bridge_count_adj = (built_bridges / total_area * 10000),
+         built_perc_poor_bridges = built_perc_poor_bridges, 
+         road_count_adj = (road_count / total_area),
+         miles_of_road_adj = (miles_of_road / total_area),
+         # educational facilities
+         publicschool_adj = (publicschool_count / COUNTYPOP * 10000), 
+         privateschool_adj = (privateschool_count / COUNTYPOP * 10000),
+         university_adj = (university_count / COUNTYPOP * 10000 * 10000),
+         suppcollege_adj = (suppcollege_count / COUNTYPOP * 10000 * 10000),
+         built_educ_facs = ((publicschool_count + privateschool_count + university_count + suppcollege_count) / COUNTYPOP * 10000), 
+         # emergency facilities 
+         hospitals_adj = (hospital_count / COUNTYPOP * 10000),
+         urgentcares_count = (urgentcare_count / COUNTYPOP * 10000),
+         mentalhealthfacs_adj = (mentalhealth_facs / COUNTYPOP * 10000),
+         fire_stations_adj = (fire_station_count / COUNTYPOP * 10000),
+         localpolice_adj = (localpolice_count / COUNTYPOP * 10000),
+         built_emergency_facs = ((hospital_count + psap_count + urgentcare_count + vahealth_count + ems_stations_count +
+                      nhsc_facs + mentalhealth_facs + dental_facs + pcp_facs +  fire_station_count + localpolice_count) / COUNTYPOP * 10000),
+         # convention facilities 
+         placesofworship_adj = (placesofworship_count / COUNTYPOP * 10000),
+         fairgrounds_adj = (fairgrounds_count / COUNTYPOP * 10000),
+         sportvenues_adj =(sportvenues_count / COUNTYPOP * 10000),
+         built_convention_facs = ((placesofworship_count + fairgrounds_count + sportvenues_count) / COUNTYPOP * 10000),
+         built_energy_facs = ((electric_substations_count + power_plant_count +  ethanol_plant_count + ethanol_loading_count +
+           petro_plant_count + petro_terminal_count + biodiesel_plant_count) / COUNTYPOP  * 10000)) 
 
+data %>% 
+  select(NAME.y, COUNTYPOP, built_bridges, bridge_count_adj, built_perc_poor_bridges, road_count, road_count_adj, miles_of_road, miles_of_road_adj) %>% 
+  arrange(-bridge_count_adj)
 
-
-data <- data %>% 
-  mutate(built_emergency_facs = hospital_count + psap_count + urgentcare_count + vahealth_count + ems_stations_count +
-                                nhsc_facs + mentalhealth_facs + dental_facs + pcp_facs +  fire_station_count + localpolice_count, 
-         built_convention_facs = placesofworship_count + fairgrounds_count + sportvenues_count, 
-         built_educ_facs = publicschool_count + privateschool_count + university_count + suppcollege_count, 
-         built_energy_facs = electric_substations_count + power_plant_count +  ethanol_plant_count + ethanol_loading_count +
-                             petro_plant_count + petro_terminal_count + biodiesel_plant_count) 
-
+# LEFT OFF HERE ---> THE ROUND COUNTS DON'T MAKE SENSE IN THE CITIES, NEED TO GO BACK AND CHECK ALL THE VARS THAT WERE INCLUDED
 
 
 #
